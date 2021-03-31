88a2c8c1c4685d31d0df8860c8b2707dc15025b9
Asif Hoque <asif.engr13@gmail.com>
2021-03-31 13:07:25 +0200

    largestOfAr

--- src/codingchallenge/largestOfArr.js
+++ src/codingchallenge/largestOfArr.js
@@ -1,11 +1,20 @@
-function largestOfArr(arr1, arr2, arr3, arr4) {
-    if(arr1 === []) {
+function largestOfArr(arr) {
+    if(arr === []) {
         return arr1;
     }
     //check
 
-    
-
+    var max = [];
+    arr.map( eachArr => {
+        eachArr.map( eachItem => {
+            let maxItem = 0;
+            if(eachItem > maxItem) {
+                maxItem = eachItem
+            }
+            console.log(maxItem)
+        })
+    })
+    return max;
 }
 
 largestOfArr([[4,5,1,3],[13,27,18,26],[32,35,37,39],[1000,1001,857,1]])
