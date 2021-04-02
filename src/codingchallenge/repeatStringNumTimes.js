f06a67b796b1b8a14d95bccd22d4f4df3fe0e68d
Asif Hoque <asif.engr13@gmail.com>
2021-04-02 21:57:55 +0200

    repeatStringNumTimes

--- src/codingchallenge/repeatStringNumTimes.js
+++ src/codingchallenge/repeatStringNumTimes.js
@@ -4,3 +4,26 @@ function repeatStringNumTimes(str, num) {
     return str.repeat(num);
 }
 repeatStringNumTimes("abc", 3)
+
+// sol 2
+function repeatStringNumTimes(str, num) {
+    //check
+    if(num < 0) return "";
+
+    var final = "";
+    for(var i=0; i<num; i++){
+        final += str;
+    }
+    return final;
+}
+repeatStringNumTimes("abc", 3)
+
+//sol 3 - recursion 
+function repeatStringNumTimes(str, num) { 
+    //check
+    if(num < 0) return "";
+
+    if(num === 1) return str; //base case
+    return str + repeatStringNumTimes(str, num - 1)
+}
+repeatStringNumTimes("abc", 3)
