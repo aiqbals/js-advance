b2be19532e9ef28a2764e1838f1452149151a7ca
Asif Hoque <asif.engr13@gmail.com>
2021-04-03 22:19:12 +0200

    chunkArrayInGroup

---
+++ src/codingchallenge/slasher.js
@@ -0,0 +1,7 @@
+function slasher(arr, howMany) {
+    //check
+    if(arr.length === 0) return 'not enough element to chop';
+    if(arr.length < howMany) return 'large chopping size';
+
+}
+slasher([1,2,3], howMany)
