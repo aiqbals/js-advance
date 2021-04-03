768a84dfc35872b5ecd046747fd25ce0e892d947
Asif Hoque <asif.engr13@gmail.com>
2021-04-03 22:30:27 +0200

    chopping

--- src/codingchallenge/slasher.js
+++ src/codingchallenge/slasher.js
@@ -2,11 +2,9 @@ function slasher(arr, howMany) {
     //check
     if(arr.length === 0) return 'not enough element to chop';
     //if(arr.length < howMany) return 'large chopping size';
-    if(arr.length < howMany) {
-        arr.splice(0)
-    } else {
-        arr.splice(0, howMany);
-    }
+    arr.splice(0, howMany)
     return arr;
+   
 }
 slasher([1,2,3], 2)
+slasher(["a","b","c"], 2)
