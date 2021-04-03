5b8da05d08b647110bc6e054eac76dc56ffd2c81
Asif Hoque <asif.engr13@gmail.com>
2021-04-03 21:58:21 +0200

    chunkArrayInGroup

--- src/codingchallenge/chunkArrayInGroups.js
+++ src/codingchallenge/chunkArrayInGroups.js
@@ -1,4 +1,14 @@
 function chunkArrayInGroups(arr, size) {
+    //check
+    if(arr.length === 0 ) {
+        return "incorrect input"
+    }
 
+    var groups = [];
+    while (arr.length > 0) {
+
+    }
+
+    return groups;
 }
 chunkArrayInGroups(["a", "b", "c", "d"], 2)
