31ff25dbc7c8abd8256578ec5ca88d7fe3d1240f
Asif Hoque <asif.engr13@gmail.com>
2021-04-05 20:23:16 +0200

    bouncer

--- src/codingchallenge/bouncer.js
+++ src/codingchallenge/bouncer.js
@@ -1,29 +1,33 @@
 //Falsy value in JS - false, null, 0, "", undefined, NaN
-//if("") console.log('not falsy') // will output undefined
+//if("") console.log('not falsy') // will output undefined, but if there is truthy val, it works
 
 function bouncer(arr) {
-    const fltr = [];
-
+    
+    /* const fltr = [];
     arr.filter( e => {
         if(!e === false || null || 0 || "" || undefined || NaN) {
             fltr.push(e)
         }
     })
+    return fltr; */
 
-    return fltr;
+    return arr.filter(e => {
+        return e
+        // if element is truty val, it will return the array with truthy val
+    })
+
 }
 bouncer([7, "ate", "", false, 9]);
 
 //sol 2
 function bouncer(arr) {
-    const fltr = [];
+    const truthies = [];
 
-    arr.filter( e => {
-        if(!e === false || null || 0 || "" || undefined || NaN) {
-            fltr.push(e)
-        }
-    })
+    for(var item of arr) {
+        if(item)
+        truthies.push(item)
+    }
 
-    return fltr;
+    return truthies;
 }
 bouncer([7, "ate", "", false, 9]);
