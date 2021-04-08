7df2690c23f574c8d25cc3b79075babb8e36201d
Asif Hoque <asif.engr13@gmail.com>
2021-04-08 11:42:24 +0200

    getIndexToIns

--- src/codingchallenge/getIndexToIns.js
+++ src/codingchallenge/getIndexToIns.js
@@ -1,4 +1,14 @@
 function getIndexToIns(arr, num) {
     //check
+    arr.sort( (a,b) => {
+        return a-b //accending order
+    }); // sort ['a','c','b', 19, 2] -- JS sort alphabetically and it counts the first digit, so 19 comes first.
+    
+    return arr.filter(e => {
+        if (num <= e) {
+            return indexOf(e)
+        }
+    })
+
 }
 getIndexToIns([40, 60], 50);
