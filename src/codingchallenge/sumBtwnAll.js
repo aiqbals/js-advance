475a8b4c13e54467db15f16a3037511ded24bf81
Asif Hoque <asif.engr13@gmail.com>
2021-04-09 11:33:41 +0200

    sumAll

--- src/codingchallenge/sumBtwnAll.js
+++ src/codingchallenge/sumBtwnAll.js
@@ -12,21 +12,6 @@ function sumBtwnAll(arr) {
 }
 sumAll([1,4])
 
-//sol 2
-function sumBtwnAll(arr) {
-    // check input validty
-    var start = Math.min(arr[0], arr[1]);
-    var end = Math.max(arr[0], arr[1]);
-    //console.log(start, end);
-    var total = 0;
-    for(var i = start; i <= end; i++) {
-        total += i;
-    }
-
-    return total;
-}
-sumAll([1,4])
-
 
 //sumAll
 function sumAll(arr) {
