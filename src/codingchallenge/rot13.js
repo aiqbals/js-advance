ea49b5bc68a67397e99ed126dc471819e226b53a
Asif Hoque <asif.engr13@gmail.com>
2021-04-09 10:27:40 +0200

    rot13

--- src/codingchallenge/rot13.js
+++ src/codingchallenge/rot13.js
@@ -1,11 +1,14 @@
 function rot13(str) {
     //check input validty
-    
+    var solved = "";
     for(var i =0; i < str.length; i++) {
         var asciiNum = str[i].charCodeAt()
         //console.log(asciiNum)
+        if(asciiNum >= 65 && asciiNum <= 90) {
+            solved += String.fromCharCode(asciiNum + 13);
+        }
     }
 }
 
-rot13("A")
+rot13("AZ")
 //rot13("SERR PBQR PNZC")
