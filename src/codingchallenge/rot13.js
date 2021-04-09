1cd7af8faf4c7f2d2a2b594d48f1e1f80a49476a
Asif Hoque <asif.engr13@gmail.com>
2021-04-09 10:24:13 +0200

    rot13

--- src/codingchallenge/rot13.js
+++ src/codingchallenge/rot13.js
@@ -1,4 +1,11 @@
-function rot13() {
+function rot13(str) {
     //check input validty
+    
+    for(var i =0; i < str.length; i++) {
+        var asciiNum = str[i].charCodeAt()
+        //console.log(asciiNum)
+    }
 }
-rot("SERR PBQR PNZC")
+
+rot13("A")
+//rot13("SERR PBQR PNZC")
