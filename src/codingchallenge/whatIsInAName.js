489daaf74407fc1da0f61fd65a4bee34b8ff3500
Asif Hoque <asif.engr13@gmail.com>
2021-04-14 12:32:57 +0200

    whatIsInAName

---
+++ src/codingchallenge/whatIsInAName.js
@@ -0,0 +1,5 @@
+function whatIsInAName(collection, source) {
+    //check 
+    
+}
+whatIsInAName([{ first: "Azief", last: "Hoque"}, { first: "Aydin", last: null}, { first: "Lany", last:"Pinky"}], {last: "Pinky"});
