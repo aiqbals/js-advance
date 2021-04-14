edae726c14c3f78002e82f198056299f31c6f540
Asif Hoque <asif.engr13@gmail.com>
2021-04-14 12:48:28 +0200

    whatIsInAName

--- src/codingchallenge/whatIsInAName.js
+++ src/codingchallenge/whatIsInAName.js
@@ -1,5 +1,13 @@
 function whatIsInAName(collection, source) {
     //check 
-    
+    let arr = [];
+    let keys = Object.keys(source);
+    //console.log(keys)
+    Object.keys(collection).forEach( element => {
+        if(element.hasOwnProperty(keys)) 
+        return arr.push(element)
+    })
+
+    return arr;
 }
 whatIsInAName([{ first: "Azief", last: "Hoque"}, { first: "Aydin", last: null}, { first: "Lany", last:"Pinky"}], {last: "Pinky"});
