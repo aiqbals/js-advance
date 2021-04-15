d0337901988e4981031878a5c152273b405b833b
Asif Hoque <asif.engr13@gmail.com>
2021-04-15 16:05:10 +0200

    translatePlgLatin

--- src/codingchallenge/translatePlgLatin.js
+++ src/codingchallenge/translatePlgLatin.js
@@ -1,27 +1,24 @@
 function translatePlgLatin(str) {
     //check
-    let tst = str.split("");
-    for(let e of tst) {
-        if(e==='a' || e==='e'|| e==='i' || e==='o'|| e==='u')
-        let cons = tst.splice(e,1)
-        tst = tst.concat(cons) + 'ay'
-        break
-    }
-    return  tst.join("")
-}
-translatePlgLatin("consonant");
-
-//sol 2
-function translatePlgLatin(str) {
-    //check
     
-    var firstVal = str.match(/[aeiou]/);
+    //var firstVal = str.match(/[aeiou]/);
     //console.log(firstVal)
-    var firstvalPos = str.indexOf(firstVal);
+    //var firstvalPos = str.indexOf(firstVal);
+    var firstvalPos = findFirstVowelPos(str);
 
     if(firstvalPos > 0) {
-        return str.slice(firsvalpos) + str.slice(0, firstvalPos) + 'ay'
+        return str.slice(firstvalPos) + str.slice(0, firstvalPos) + 'ay'
     }
     return str + 'way';
 }
 translatePlgLatin("consonant");
+
+// our fn to find the index, not using regX
+function findFirstVowelPos(str) {
+    //check
+    for(let i=0; i<str.length; i++) {
+        if('aeiou'.indexOf(str[i]) !== -1) {
+            return i;
+        }
+    }
+}
