882eb3958755f64aa4810d14d881f52f6ff96a5f
Asif Hoque <asif.engr13@gmail.com>
2021-04-15 15:22:00 +0200

    myReplace

--- src/codingchallenge/myReplace.js
+++ src/codingchallenge/myReplace.js
@@ -1,5 +1,9 @@
 function myReplace(str, before, after) {
-    //check  
+    //check 
+    if(before[0]===before[0].toUpperCase()) {
+        after = after[0].toUpperCase() + after.slice(1);
+    }
+    
     const strAr = str.split(" ");
     const index = strAr.indexOf(before);
     strAr.filter( e => {
@@ -14,13 +18,27 @@ myReplace("A quick brown fox jumped over the lazy dog", "jumped", "leaped")
 // sol 2
 function myReplace(str, before, after) {
     //check  
-    let strAr = str.split(" ");
+    if(before[0]===before[0].toUpperCase()) {
+        after = after[0].toUpperCase() + after.slice(1);
+    }
 
+    let strAr = str.split(" ");
     for(var el of strAr) {
         if(el==before)
         strAr.splice(strAr.indexOf(before), 1, after)
     }
     return strAr
+}
+
+myReplace("A quick brown fox jumped over the lazy dog", "jumped", "leaped")
+
+//sol 3
+function myReplace(str, before, after) {
+    //check  
+    if(before[0]===before[0].toUpperCase()) {
+        after = after[0].toUpperCase() + after.slice(1);
+    }
+    return str.replace(before, after)
 }
 
 myReplace("A quick brown fox jumped over the lazy dog", "jumped", "leaped")
