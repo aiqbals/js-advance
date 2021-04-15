5bf49fc61edbe5f8713bd2cd7a0711646f115ae9
Asif Hoque <asif.engr13@gmail.com>
2021-04-15 15:14:24 +0200

    myReplace

--- src/codingchallenge/myReplace.js
+++ src/codingchallenge/myReplace.js
@@ -2,25 +2,25 @@ function myReplace(str, before, after) {
     //check  
     const strAr = str.split(" ");
     const index = strAr.indexOf(before);
-    return strAr.filter( e => {
+    strAr.filter( e => {
         if(e===before)
-        strAr.splice(index,1,after);
-        return true;
+        strAr.splice(index,1,after)
     })
+    return strAr
+}
 
 myReplace("A quick brown fox jumped over the lazy dog", "jumped", "leaped")
 
 // sol 2
 function myReplace(str, before, after) {
     //check  
-    const strAr = str.split(" ");
-    return strAr.filter( e => {
-        const index = strAr.indexOf(before);
-        if(e===before)
-        //console.log(strAr.indexOf(before))
-        return strAr.splice(index,1,after);
-        //e.replace(after)
-    })
+    let strAr = str.split(" ");
+
+    for(var el of strAr) {
+        if(el==before)
+        strAr.splice(strAr.indexOf(before), 1, after)
+    }
+    return strAr
 }
 
 myReplace("A quick brown fox jumped over the lazy dog", "jumped", "leaped")
