9eb03c9229a977ba8b645be14b7df8ecc52e0d6b
Asif Hoque <asif.engr13@gmail.com>
2021-04-15 15:40:07 +0200

    translatePlgLatin

--- src/codingchallenge/translatePlgLatin.js
+++ src/codingchallenge/translatePlgLatin.js
@@ -1,4 +1,12 @@
 function translatePlgLatin(str) {
     //check
+    let tst = str.split("");
+    for(let e of tst) {
+        if(e==='a' || e==='e'|| e==='i' || e==='o'|| e==='u')
+        let cons = tst.splice(e,1)
+        tst = tst.concat(cons) + 'ay'
+        break
+    }
+    return  tst
 }
 translatePlgLatin("consonant");
