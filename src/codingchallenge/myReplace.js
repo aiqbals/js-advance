700f3ab500948bc30596a94d2888d8dfa879be94
Asif Hoque <asif.engr13@gmail.com>
2021-04-15 15:26:41 +0200

    myReplace

--- src/codingchallenge/myReplace.js
+++ src/codingchallenge/myReplace.js
@@ -3,7 +3,7 @@ function myReplace(str, before, after) {
     if(before[0]===before[0].toUpperCase()) {
         after = after[0].toUpperCase() + after.slice(1);
     }
-    
+
     const strAr = str.split(" ");
     const index = strAr.indexOf(before);
     strAr.filter( e => {
@@ -33,6 +33,24 @@ function myReplace(str, before, after) {
 myReplace("A quick brown fox jumped over the lazy dog", "jumped", "leaped")
 
 //sol 3
+function myReplace(str, before, after) {
+    //check  
+    const words = str.split(" ");
+    if(before[0]===before[0].toUpperCase()) {
+        after = after[0].toUpperCase() + after.slice(1);
+    }
+
+    for(let i= 0; i<words.length; i++) {
+        if(words[i]===before) {
+            words[i] = after
+        }
+    }
+    return words
+}
+
+myReplace("A quick brown fox jumped over the lazy dog", "jumped", "leaped")
+
+// sol 4
 function myReplace(str, before, after) {
     //check  
     if(before[0]===before[0].toUpperCase()) {
