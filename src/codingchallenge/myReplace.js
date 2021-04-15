0bfb62eaf0e63b55d900921a239263fed1fd0f38
Asif Hoque <asif.engr13@gmail.com>
2021-04-15 15:08:45 +0200

    myReplace

--- src/codingchallenge/myReplace.js
+++ src/codingchallenge/myReplace.js
@@ -1,6 +1,19 @@
 function myReplace(str, before, after) {
     //check  
     const strAr = str.split(" ");
+    const index = strAr.indexOf(before);
+    return strAr.filter( e => {
+        if(e===before)
+        strAr.splice(index,1,after);
+        return true;
+    })
+
+myReplace("A quick brown fox jumped over the lazy dog", "jumped", "leaped")
+
+// sol 2
+function myReplace(str, before, after) {
+    //check  
+    const strAr = str.split(" ");
     return strAr.filter( e => {
         const index = strAr.indexOf(before);
         if(e===before)
