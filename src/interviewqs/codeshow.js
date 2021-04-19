888bd5975f4459c9bac1ec2b0d41338a719ebd62
Asif Hoque <asif.engr13@gmail.com>
2021-04-19 12:32:39 +0200

    interviewqs

--- src/interviewqs/codeshow.js
+++ src/interviewqs/codeshow.js
@@ -18,5 +18,51 @@ function a() {
 } 
 console.log('hi') // we get it
 
-let a = () => arguments; // we dont get it using arrow fn
-console.log('hi')
+let a = () => arguments; 
+console.log('hi') 
+// we dont get it using arrow fn cuz arguments doesnt bind to arr fn variable. 
+// So dont sue arg in arr fn rather use 
+let a = (...n) => n; 
+console.log('hi') 
+
+
+5. 
+let x = function() {
+    return 
+    {
+        message: 'hi'
+    }
+}
+console.log(x());
+// wont work cuz of line braking in return statement cuz js finds it as return ; which is return nothing
+
+6.1 
+let profile = {
+    name: 'hoque'
+}
+// to avaoid adding new properties
+Object.freeze(profile);
+
+// to modify any existing property without adding age property - use seal
+Object.seal(profile)
+
+profile.name = 'azief'; // will not be added to modify when use freeze but seal
+profile.age = 3; // will not be added when use freeze and seal
+
+console.log(profile);
+
+6.2
+let profile = {
+    name: 'hoque',
+    age: 3
+}
+// to modify name and adding property age which is not modifyable 
+Object.defineProperties(profile, 'age', {
+    value: 3,
+    writable: false
+})
+
+profile.name = 'Azief';
+profile.age = 3;
+
+console.log(profile);
